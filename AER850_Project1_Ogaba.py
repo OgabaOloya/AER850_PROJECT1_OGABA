baedb48b1cdaf9b1025086c4049d10f95357678b
OgabaOloya <ooloya@torontomu.ca>
2025-09-29 23:13:53 -0400

    Starting code
    First commit

---
+++ AER850_Project1_Ogaba.py
@@ -0,0 +1,13 @@
+# ================================
+#Ogaba Oloya
+#AER 850 Project 1
+#501097689
+
+#------------------------------------------------------------------------------
+#Step 1: Data Processing 
+
+#Importing our relevant toolkits into the system for usage later
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+data = pd.read_csv("data/Project 1 Data.csv")
