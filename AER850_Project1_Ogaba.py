0c9acc40054a05711e84950d6bd30ea03f66411a
OgabaOloya <ooloya@torontomu.ca>
2025-09-30 01:54:43 -0400

    Update AER850_Project1_Ogaba.py
    Finished Q1 and Q2

--- AER850_Project1_Ogaba.py
+++ AER850_Project1_Ogaba.py
@@ -30,3 +30,34 @@ print(data.head())
 #Step 2: Data Visualization 
 print("\n\n-----------------Step 2: Data Visualization-----------------\n\n")
 
+#Setting up boxplot subplots for X, Y, and Z
+plt.figure(figsize=(15,5))
+
+#Box plot for the variable X
+plt.subplot(1,3,1)
+data.boxplot(column='X',by='Step',grid=False)
+plt.title('Distribution of X per Step')
+
+#Removing of the automatic title which will be applied to Y and Z plots
+plt.suptitle('')
+plt.xlabel('Step')
+plt.ylabel('X')
+
+#Box plot for the variable Y
+plt.subplot(1,3,2)
+data.boxplot(column='Y',by='Step',grid=False)
+plt.title('Distribution of Y per Step')
+plt.suptitle('')
+plt.xlabel('Step')
+plt.ylabel('Y')
+
+#Box plot for the variable Z
+plt.subplot(1,3,3)
+data.boxplot(column='Z',by='Step',grid=False)
+plt.title('Distribution of Z per Step')
+plt.suptitle('')
+plt.xlabel('Step')
+plt.ylabel('Z')
+
+plt.tight_layout()
+plt.show()
